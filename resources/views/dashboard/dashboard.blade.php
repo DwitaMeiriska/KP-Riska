65d738b4de2bd0d80ffe117b20c4a67cf76cb3b1
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-10-10 07:34:07 +0700

    add: ppdb

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,20 +7,22 @@
             <button type="button" data-bs-target="#carouselDekstop" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselDekstop" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-45.jpg" class="d-block w-100"
-                    alt="...">
+                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-45.jpg" class="d-block w-100 rounded"
+                    alt="..." style="max-height: 400px; object-fit: cover;">
             </div>
             <div class="carousel-item">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-76.jpg" class="d-block w-100"
-                    alt="...">
+                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-76.jpg" class="d-block w-100 rounded"
+                    alt="..." style="max-height: 400px; object-fit: cover;">
             </div>
             <div class="carousel-item">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-86.jpg" class="d-block w-100"
-                    alt="...">
+                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/bg-edukasi-86.jpg" class="d-block w-100 rounded"
+                    alt="..." style="max-height: 400px; object-fit: cover;">
             </div>
         </div>
+
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselDekstop" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
